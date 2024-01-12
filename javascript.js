b4bf79f6d637bfec3aa02a80276dc4b1b5154d2b
Isaacluka <lukaisaacabdulkarim@gmail.com>
2024-01-12 16:06:02 +0100

    First Commit

--- javascript.js
+++ javascript.js
@@ -1,3 +1,69 @@
+const gameScreen = document.querySelector('#gameScreen');
+
+const sect1 = document.createElement('div');
+const sect2 = document.createElement('div');
+const sect3 = document.createElement('div');
+const sect4 = document.createElement('div');
+sect4.setAttribute('style','display: flex;','','align-items: center;','gap: 2rem;');
+sect4.style.display = "flex";
+sect4.style.gap = "2rem";
+sect4.style.justifyContent = "space-between";
+sect4.style.alignItems = "center";
+
+
+const askToPlay = document.createElement('h1');
+const caption = document.createElement('h3');
+const comment = document.createElement('h3')
+
+askToPlay.textContent = "Shall we play a game of Rock Paper Scissors?";
+askToPlay.setAttribute('style', 'color: #1a2529;');
+
+caption.textContent = "First to 5 Wins!";
+caption.setAttribute('style', 'color: brown;');
+
+const btny = document.createElement('button');
+const btnn = document.createElement('button');
+
+btny.textContent = "Yes";
+btnn.textContent = "Nope";
+
+const btnR = document.createElement('button');
+const btnP = document.createElement('button');
+const btnS = document.createElement('button');
+
+btnR.textContent = "ROCK";
+btnP.textContent = "PAPER";
+btnS.textContent = "SCISSORS";
+
+btnn.setAttribute('style', 'color: white;')
+btny.setAttribute('style', 'color: white;')
+
+var playerScore = 0;
+var computerScore = 0;
+
+
+
+btnn.addEventListener('click', () => {
+    askToPlay.textContent = 'PLEASE!!!!!!!';
+    caption.textContent = "";
+    
+});
+btny.addEventListener('click', () =>{
+    playerScore = 0;
+    computerScore = 0;
+    askToPlay.textContent = playerScore + " : " + computerScore;
+    caption.textContent = "YOU \t\t\t ME";
+    comment.textContent = "Ready when you are!";
+    btnn.remove();
+    btny.remove();
+
+    sect4.appendChild(btnR);
+    sect4.appendChild(btnP);
+    sect4.appendChild(btnS);
+
+
+});
+
 function getComputerChoice(){
     let x = Math.random() * 100;
     let choice;
@@ -13,7 +79,6 @@ function getComputerChoice(){
     console.log(choice);
     return choice;
 }
-
 function playRound(playerSelection, computerSelection){
     let stat;
     
@@ -23,51 +88,123 @@ function playRound(playerSelection, computerSelection){
     }else if ((playerSelection === "rock" && computerSelection === "scissors") || 
     (playerSelection === "paper" && computerSelection === "rock") || 
     (playerSelection === "scissors" && computerSelection === "paper")){
-        console.log('You win! ' + playerSelection +' beats ' + computerSelection);
+        comment.textContent = 'You win! ' + playerSelection +' beats ' + computerSelection;
         stat = "win";
         
     }else if(playerSelection === computerSelection.toLowerCase()){
-        console.log("Let's try again!");
+        comment.textContent = "It's a draw. Let's try again!";
         stat = "try again";
     }else {
-        console.log('You lose! '+ computerSelection + ' beats ' + playerSelection);
+        comment.textContent = 'You lose! '+ computerSelection + ' beats ' + playerSelection
         stat = "loss";
     }
-    console.log(stat)
     return stat;
     
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    const status = playRound();
-    for (let i = 0; i < 3; i++){
 
-        const computerSelection = getComputerChoice().toLowerCase();
+function game(playerSelection){
+    //let playerSelection;
 
-        const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+    const computerSelection = getComputerChoice().toLowerCase();
+       // const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+         
+    console.log(playRound(playerSelection, computerSelection));
+    const status = playRound(playerSelection, computerSelection);
         
-        console.log(playRound(playerSelection, computerSelection));
-        
-        if (status == "win"){
-            playerScore = playerScore + 1;
-            return playerScore;
+    if ( status== "win"){
+        playerScore++;  
             
-        }else if (status == "loss"){
-            computerScore = computerScore +1;
-            return computerScore;
-            
-        }
-        
-    }
+    }else if (status == "loss"){
+        computerScore++;            
+    };    
+    if (playerScore == 5){
+
+        btnR.remove();  
+        btnP.remove(); 
+        btnS.remove(); 
+        comment.textContent = "CONGRATULATIONS!!! You Win!";  
+        btny.textContent = "Play again?";
+        sect4.appendChild(btny);        
+    }else if (computerScore == 5){
+        btnR.remove();  
+        btnP.remove(); 
+        btnS.remove();
+        comment.textContent = "Yay!!! I Win, You Lose!!";
+        btny.textContent = "Play again?";
+        sect4.appendChild(btny);
+    };
+     
+    
+    return [playerScore, computerScore];
     
     
-
-    console.log(playerScore);
-    console.log(computerScore);
-}
+};
 
 
-game();
+
+btnR.addEventListener('click', () =>{
+    const playerSelection = 'rock';
+    game('rock')
+    askToPlay.textContent = playerScore + " : " + computerScore;
+    
+    
+});
+        
+btnP.addEventListener('click', () =>{
+    game('paper')
+    askToPlay.textContent = playerScore + " : " + computerScore;
+    if (playerScore == 5){
+
+        btnR.remove();  
+        btnP.remove(); 
+        btnS.remove(); 
+        comment.textContent = "CONGRATULATIONS!!! You Win!";  
+        btny.textContent = "Play again?";
+        sect4.appendChild(btny);
+    }else if (computerScore == 5){
+        btnR.remove();  
+        btnP.remove(); 
+        btnS.remove();
+        comment.textContent = "Yay!!! I Win, You Lose!!";
+        btny.textContent = "Play again?";
+        sect4.appendChild(btny);
+    };
+    
+});
+
+btnS.addEventListener('click', () =>{
+    game('scissors')
+    askToPlay.textContent = playerScore + " : " + computerScore;
+    if (playerScore == 5){
+
+        btnR.remove();  
+        btnP.remove(); 
+        btnS.remove(); 
+        comment.textContent = "CONGRATULATIONS!!! You Win!";  
+        btny.textContent = "Play again?";
+        sect4.appendChild(btny);
+    }else if (computerScore == 5){
+        btnR.remove();  
+        btnP.remove(); 
+        btnS.remove();
+        comment.textContent = "Yay!!! I Win, You Lose!!";
+        btny.textContent = "Play again?";
+        sect4.appendChild(btny);
+    };
+
+});
+
+sect1.appendChild(askToPlay);
+sect2.appendChild(caption);
+sect3.appendChild(comment);
+sect4.appendChild(btny);
+sect4.appendChild(btnn);
+
+gameScreen.appendChild(sect1);
+gameScreen.appendChild(sect2);
+gameScreen.appendChild(sect3);
+gameScreen.appendChild(sect4);
+
+
 
